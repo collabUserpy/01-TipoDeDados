0c4aa66b2959eeb41061758aae20adb10bc57af6
Usercollabpy <pythondozerodrive@gmail.com>
2026-09-15 19:37:26 -0300

    AtividadeClaudio02

--- 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
+++ 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
@@ -8,10 +8,10 @@ int main()
 {
 	cout << "Estrutura de Dados - Fatec (2026/2)" << endl;
 
-	
+
 	cout << "Tipos Numericos\n";
 	cout << "===============\n";
-	
+
 	int i = 10;
 	short s = 18;
 	long l = 200000;
@@ -66,7 +66,7 @@ int main()
 	cin >> idade;
 	dias = idade * 365;
 	horas = dias * 24;
-	minutos = horas  * 60;
+	minutos = horas * 60;
 	cout << "Sua idade em dias e: " << dias << endl;
 	cout << "Sua idade em horas e: " << horas << endl;
 	cout << "Sua idade em minutos e: " << minutos << endl;
@@ -74,6 +74,55 @@ int main()
 	cout << "Qualquer tecla para sair...";
 	system("pause"); // somente no Windows
 
+	/*EXERCICIO 1*/
+	#include <climits>
+	cout << "int  max = " << INT_MAX << "\n";
+	cout << "int  min = " << INT_MIN << "\n";
+	cout << "short max = " << SHRT_MAX << "\n";
+	cout << "int  max = " << INT_MAX + 1LL << "\n";
+
+	/*Pergunta: O que acontece quando você soma INT_MAX + 1?
+	Ele ultrapassa o maior valor que um int pode mostrar, dando uma sobrecarca e printando um valor negativo. */
+
+	/*EXERCICIO 2*/
+
+	double temperaturaCelsius = 0;
+
+	cout << "Digite uma temperatura em Celsius: ";
+	cin >> temperaturaCelsius;
+
+	double temperaturaFahrenheit = temperaturaCelsius * 1.8 + 32;
+
+	cout << "A temperatura em Fahrenheit e:" << temperaturaFahrenheit << endl;
+
+	double temperaturaKelvin = temperaturaCelsius + 273.15;
+
+	cout << "A temperatura em Kelvin e:" << temperaturaKelvin << endl;
+
+
+	/*EXERCICIO 3*/
+
+	long segundosRestantes = 0;
+	cout << "Digite	o numero de segundos: ";
+	cin >> segundosRestantes;
+
+	long restoDAY = segundosRestantes / 86400;
+	segundosRestantes = segundosRestantes % 86400;
+
+
+	long restoHOUR = segundosRestantes / 3600;
+	segundosRestantes = segundosRestantes % 3600;
+
+
+	long restoMIN = segundosRestantes / 60;
+
+
+	long restoSEC = segundosRestantes % 60;
+
+	cout << "Dias: " << restoDAY << endl;
+	cout << "Horas: " << restoHOUR << endl;
+	cout << "Minutos: " << restoMIN << endl;
+	cout << "Segundos: " << restoSEC << endl;
 
 }
 
